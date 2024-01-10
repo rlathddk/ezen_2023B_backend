b5a5f18c34ea6bf633c5af6abc2cfa8a7fc53503
[girlathddk <[giabc777z@naver.com>
2024-01-10 14:04:42 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/개인과제/과제2/ex1/Level2_1.PNG
    #	src/개인과제/과제2/ex1/Level2_1.java
    #	src/개인과제/과제2/ex2/Level2_2.PNG
    #	src/개인과제/과제2/ex2/Level2_2.java
    #	src/개인과제/과제2/ex3/Level2_3.PNG
    #	src/개인과제/과제2/ex3/Level2_3.java
    #	src/개인과제/과제2/ex4/Level2_4.java
    #	src/개인과제/과제2/ex4/Level2_4_1.PNG
    #	src/개인과제/과제2/ex4/Level2_4_2.PNG
    #	src/개인과제/과제3/ex1/Level3_1.PNG
    #	src/개인과제/과제3/ex1/Level3_1.java
    #	src/개인과제/과제3/ex2/Level3_2.PNG
    #	src/개인과제/과제3/ex2/Level3_2.java
    #	src/개인과제/과제3/ex3/Level3_3.PNG
    #	src/개인과제/과제3/ex3/Level3_3.java
    #	src/개인과제/과제3/ex4/Level3_4.PNG
    #	src/개인과제/과제3/ex4/Level3_4.java
    #	src/개인과제/과제3/ex5/Level3_5.PNG
    #	src/개인과제/과제3/ex5/Level3_5.java

--- src/day07/Step2.java
+++ src/day07/Step2.java
@@ -20,5 +20,25 @@ public class Step2 { // cs
         System.out.println("모델명 : " + car.model);
         System.out.println("시동여부 : " + car.start);
         System.out.println("현재속도 : " + car.speed);
+
+        // p.217
+        // * 필드는 객체의 데이터 이므로 객체가 존재하지 않으면 필드도 존재하지 않는다
+            // 1. 객체 생성
+            Car2 myCar2 = new Car2();
+            // 2. 객체 내 필드 호출
+        System.out.println("제작회사 : " + myCar2.company);
+        System.out.println("모델명 : " + myCar2.model);
+        System.out.println("색깔 : " + myCar2.color);
+        System.out.println("최고속도 : " + myCar2.maxSpeed);
+        System.out.println("현재속도 : " + myCar2.speed);
+            // 3. 객체 내 필드의 값 수정
+        myCar2.speed = 60;
+        System.out.println("현재속도 : " + myCar2.speed);
+        // ==============
+        Car2 yourCar2 = new Car2();
+        System.out.println("제작회사 : " + yourCar2.company);
+        System.out.println("현재속도 : " + yourCar2.speed);
+
+
     } // me
 } // ce
